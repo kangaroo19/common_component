6ec9c717971fcc176fa2e86715465cda6ac5a34e
kangaroo19 <86513078+kangaroo19@users.noreply.github.com>
2024-03-04 13:34:04 +0900

    react-query 에러핸들링

--- src/pages/RQsuperheroes.page.jsx
+++ src/pages/RQsuperheroes.page.jsx
@@ -5,12 +5,15 @@ import {
 } from '../utils/query/superHeroQuery';
 // 리액트 쿼리 사용할 컴포넌트
 export default function RQSuperheroesPage() {
-  const { isLoading, data } = useQuery({
+  const { isLoading, data, isError, error } = useQuery({
     queryKey: superHeroKey,
     queryFn: getSuperHerosListFn,
   });
   if (isLoading) {
     return <h2>로딩중</h2>;
+  }
+  if (isError) {
+    return <h2>{error.message}</h2>;
   }
   return (
     <>
--- src/pages/Superheroes.page.jsx
+++ src/pages/Superheroes.page.jsx
@@ -5,17 +5,27 @@ import { useEffect, useState } from 'react';
 export default function SuperheroesPage() {
   const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState([]);
+  const [error, setError] = useState('');
 
   // 우리가 흔히 useState,useEffect을 사용하던 방식의 get 요청
   useEffect(() => {
-    axios.get('http://localhost:4000/superheroes').then((res) => {
-      setData(res.data);
-      setIsLoading(false);
-    });
+    axios
+      .get('http://localhost:4000/superher1oes')
+      .then((res) => {
+        setData(res.data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setIsLoading(false);
+      });
   }, []);
 
   if (isLoading) {
     return <h2>Loading...</h2>;
+  }
+  if (error) {
+    return <h2>{error}</h2>;
   }
   return (
     <>
