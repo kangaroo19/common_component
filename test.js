e33b6771b854c1aabd5fbfef97e2e27d314f0f53
kangaroo19 <86513078+kangaroo19@users.noreply.github.com>
2024-03-11 11:26:23 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,3 @@
-var a = 1;
-var outer = function () {
-  var inner = function () {
-    console.log(a);
-    var a = 3;
-  };
-  inner();
-  console.log(a);
-};
-
-outer();
-console.log(a);
+setTimeout(function () {
+  console.log(this);
+});
